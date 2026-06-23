389619e879727244af01a15a9e11008f25ed5c24
louisvb27 <louis2711ls@gmail.com>
2026-06-23 07:57:14 -0700

    agregando nodos

--- ArbolesGenerales/Arbol.cs
+++ ArbolesGenerales/Arbol.cs
@@ -7,5 +7,34 @@
         {
             Raiz = new Nodo(raiz);
         }
+
+        public Nodo Agregar(string valor, Nodo nodoPadre)
+        {
+            if (string.IsNullOrWhiteSpace(valor))
+            {
+                throw new Exception("el valor es un dato requerido");
+            }
+
+            if (nodoPadre == null)
+            {
+                throw new Exception("el nodo padre es un dato requerido");
+            }
+
+            if (nodoPadre.Hijo == null)
+            {
+                nodoPadre.Hijo = new Nodo(valor);
+                return nodoPadre.Hijo;
+            }
+            else
+            {
+                Nodo nodoActual = nodoPadre.Hijo;
+                while (nodoActual.Hermano != null)
+                {
+                    nodoActual = nodoActual.Hermano;
+                }
+                nodoActual.Hermano = new Nodo(valor);
+                return nodoActual.Hermano;
+            }
+        }
     }
 }
