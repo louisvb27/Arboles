e69a23817cf3197310eab6d7e2279d9ade6e0c9e
louisvb27 <louis2711ls@gmail.com>
2026-07-28 08:00:13 -0700

    recorridos

--- ArbolesBinarios/ArbolBinarioDeBusqueda.cs
+++ ArbolesBinarios/ArbolBinarioDeBusqueda.cs
@@ -72,4 +72,97 @@ internal class ArbolBinarioDeBusqueda
             Recorrer(nodo.Derecho, ref datos);
         }
     }
+    public string ObtenerDatos()
+    {
+        string datos = string.Empty;
+        Recorrer(Raiz, ref datos);
+        return datos;
+    }
+
+    private void Seleccionar(Nodo nodo, ref string datos) 
+    {
+        string coma = (datos == string.Empty) ? string.Empty : ",";
+        datos += $"{coma}{nodo.Valor}";
+
+    }
+
+    private void RecorridoInorden(Nodo? nodo, ref string datos)
+    {
+        if (nodo != null)
+        {
+            if (nodo.Izquierdo != null)
+            {
+                RecorridoInorden(nodo.Izquierdo, ref datos);
+            }
+
+            Seleccionar(nodo, ref datos);
+
+            if (nodo.Derecho != null)
+            {
+                RecorridoInorden(nodo.Derecho, ref datos);
+            }
+
+        }
+        
+    }
+
+    private void RecorridoPreorden(Nodo? nodo, ref string datos)
+    {
+        if (nodo != null)
+        {
+            Seleccionar(nodo, ref datos);
+
+            if (nodo.Izquierdo != null)
+            {
+                RecorridoInorden(nodo.Izquierdo, ref datos);
+            }
+
+
+            if (nodo.Derecho != null)
+            {
+                RecorridoInorden(nodo.Derecho, ref datos);
+            }
+
+        }
+
+    }
+
+    private void RecorridoPosorden(Nodo? nodo, ref string datos)
+    {
+        if (nodo != null)
+        {
+            if (nodo.Izquierdo != null)
+            {
+                RecorridoInorden(nodo.Izquierdo, ref datos);
+            }
+
+
+            if (nodo.Derecho != null)
+            {
+                RecorridoInorden(nodo.Derecho, ref datos);
+            }
+            
+            Seleccionar(nodo, ref datos);
+
+        }
+
+    }
+
+    public string ObtenerDatos(Recorrido recorrido)
+    {
+        string datos = string.Empty;
+        switch (recorrido)
+        {
+            case Recorrido.PreOrden:
+                RecorridoPreorden(Raiz, ref datos);
+                break;
+            case Recorrido.InOrden:
+                RecorridoInorden(Raiz, ref datos);
+                break;
+            case Recorrido.PostOrden:
+                RecorridoPosorden(Raiz, ref datos);
+                break;
+        }
+        return $"{recorrido}: {datos}";
+    }
 }
--- ArbolesBinarios/Program.cs
+++ ArbolesBinarios/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ArbolBinarioDeBusqueda arbol = new ArbolBinarioDeBusqueda(raiz: 10);
+            //string datos = arbol.ObtenerDatos();
+            //Console.WriteLine(datos);
+
+            arbol.Insertar(5);
+            arbol.Insertar(15);
+            arbol.Insertar(9);
+            arbol.Insertar(3);
+            //Console.WriteLine(arbol.ObtenerDatos());
+            Console.WriteLine(arbol.ObtenerDatos(ArbolBinarioDeBusqueda.Recorrido.PreOrden));
+            Console.WriteLine(arbol.ObtenerDatos(ArbolBinarioDeBusqueda.Recorrido.InOrden));
+            Console.WriteLine(arbol.ObtenerDatos(ArbolBinarioDeBusqueda.Recorrido.PostOrden));
         }
     }
 }
